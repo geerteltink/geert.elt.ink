4562bfe0b0d3c5d354fa5258e26dadcb0fbb6ab9
Geert Eltink <xtreamwayz@gmail.com>
2015-11-20 00:29:55 +0100

    Apply some fixes from pull requests

--- config/container.php
+++ config/container.php
@@ -3,11 +3,12 @@
 use Zend\ServiceManager\ServiceManager;
 
 // Load configuration
-$config = require 'config.php';
+$config = require __DIR__ . '/config.php';
 $dependencies = $config['dependencies'];
 
 // Inject config as a service
 $dependencies['services']['config'] = $config;
+$dependencies['services']['profiler'] = $profiler;
 
 // Build container
 $container = new ServiceManager($dependencies);
