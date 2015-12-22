cde1a85c3e1e6f4eed3231ab45be1ad04280c051
Geert Eltink <xtreamwayz@gmail.com>
2015-12-22 20:57:04 +0100

    Use zend-feed to build the atom feed

--- src/App/Action/BlogXmlFeed.php
+++ src/App/Action/BlogXmlFeed.php
@@ -2,13 +2,12 @@
 
 namespace App\Action;
 
-use DateTime;
 use Domain\Post\PostRepository;
-use DOMDocument;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Expressive\Router;
+use Zend\Feed\Writer\Feed;
 
 class BlogXmlFeed extends ActionAbstract
 {
@@ -32,7 +31,7 @@ class BlogXmlFeed extends ActionAbstract
 
             $item->set($feed);
         }
-        $feed = $this->generateXmlFeed();
+
         $response->getBody()->write($feed);
 
         return $response->withHeader('Content-Type', 'application/atom+xml');
@@ -40,94 +39,44 @@ class BlogXmlFeed extends ActionAbstract
 
     public function generateXmlFeed()
     {
-        $xml = new DOMDocument('1.0', 'UTF-8');
-        $xml->formatOutput = true;
-
-        $feed = $xml->createElementNS('http://www.w3.org/2005/Atom', 'feed');
-        $feed->setAttribute('xml:lang', 'en-US');
-        $xml->appendChild($feed);
-
-        $title = $xml->createElement('title', 'xtreamwayz');
-        $feed->appendChild($title);
-
-        $subtitle = $xml->createElement('subtitle', 'A web developer\'s playground, notes and thoughts.');
-        $feed->appendChild($subtitle);
-
-        $updated = $xml->createElement('updated', (new DateTime('now'))->format(DateTime::RFC3339));
-        $feed->appendChild($updated);
-
-        $selfLink = $xml->createElement('link');
-        $selfLink->setAttribute('type', 'application/atom+xml');
-        $selfLink->setAttribute('href', $this->generateUrl('feed.xml', [], true));
-        $selfLink->setAttribute('rel', 'self');
-        $feed->appendChild($selfLink);
-
-        $alternateLink = $xml->createElement('link');
-        $alternateLink->setAttribute('type', 'text/html');
-        $alternateLink->setAttribute('href', $this->generateUrl('home', [], true));
-        $alternateLink->setAttribute('rel', 'alternate');
-        $feed->appendChild($alternateLink);
-
-        $id = $xml->createElement('id', $this->generateUrl('home', [], true));
-        $feed->appendChild($id);
-
-        $generator = $xml->createElement('generator', 'zend-expressive');
-        $generator->setAttribute('uri', 'https://github.com/zendframework/zend-expressive');
-        $feed->appendChild($generator);
-
-        $rights = $xml->createElement(
-            'rights',
-            sprintf('Copyright (c) 2005-%s Geert Eltink. All Rights Reserved.', date('Y'))
+        $feed = new Feed();
+        $feed->setTitle('xtreamwayz');
+        $feed->setLink($this->generateUrl('home', [], true));
+        $feed->setFeedLink($this->generateUrl('feed.xml', [], true), 'atom');
+        $feed->addAuthor(
+            [
+                'name' => 'Geert Eltink',
+                'uri'  => 'https://xtreamwayz.com',
+            ]
         );
-        $feed->appendChild($rights);
+        $feed->setDateModified(time());
+        $feed->setCopyright(sprintf('Copyright (c) 2005-%s Geert Eltink. All Rights Reserved.', date('Y')));
+        $feed->setDescription('A web developer\'s playground, notes and thoughts.');
+        $feed->setId($this->generateUrl('home', [], true));
 
         $postRepository = $this->get(PostRepository::class);
         $posts = array_slice(array_reverse($postRepository->findAll()), 0, 5);
         /** @var \Domain\Post\Post $post */
         foreach ($posts as $post) {
-            $entry = $xml->createElement('entry');
-
-            $entryTitle = $xml->createElement('title', $post->getTitle());
-            $entry->appendChild($entryTitle);
-
-            $entryLink = $xml->createElement('link');
-            $entryLink->setAttribute('href', $this->generateUrl('blog.post', ['id' => $post->getId()], true));
-            $entry->appendChild($entryLink);
-
-            $entryId = $xml->createElement('id', $this->generateUrl('blog.post', ['id' => $post->getId()], true));
-            $entry->appendChild($entryId);
-
-            $entryPublished = $xml->createElement('published', $post->getPublished()->format(DateTime::RFC3339));
-            $entry->appendChild($entryPublished);
-
+            $entry = $feed->createEntry();
+            $entry->setTitle($post->getTitle());
+            $entry->setLink($this->generateUrl('blog.post', ['id' => $post->getId()], true));
+            $entry->setId($this->generateUrl('blog.post', ['id' => $post->getId()], true));
+            $entry->setDateCreated($post->getPublished());
             if ($post->getModified()) {
-                $entryUpdated = $xml->createElement('updated', $post->getModified()->format(DateTime::RFC3339));
-                $entry->appendChild($entryUpdated);
+                $entry->setDateModified($post->getModified());
             }
-
-            $entrySummary = $xml->createElement('summary', $post->getSummary());
-            $entry->appendChild($entrySummary);
-
-            if ($post->getContent()) {
-                $entryContent = $xml->createElement('content');
-                $entryContent->setAttribute('type', 'html');
-                $entry->appendChild($entryContent);
-                $entryContentData = $xml->createCDATASection($post->getContent());
-                $entryContent->appendChild($entryContentData);
-            }
-
-            $entryAuthor = $xml->createElement('author');
-            $entry->appendChild($entryAuthor);
-
-            $entryAuthorName = $xml->createElement('name', 'Geert Eltink');
-            $entryAuthor->appendChild($entryAuthorName);
-
-            $entryAuthorUri = $xml->createElement('uri', $this->generateUrl('home', [], true));
-            $entryAuthor->appendChild($entryAuthorUri);
-
-            $feed->appendChild($entry);
+            $entry->setDescription($post->getSummary());
+            $entry->setContent($post->getContent());
+            $entry->addAuthor(
+                [
+                    'name' => 'Geert Eltink',
+                    'uri'  => 'https://xtreamwayz.com',
+                ]
+            );
+            $feed->addEntry($entry);
         }
 
-        return $xml->saveXML();
+        return $feed->export('atom');
     }
 }
