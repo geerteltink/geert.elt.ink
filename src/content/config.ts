f3dc0feb3a8f3a4e6656f3ff181d8c81b7975ef9
Geert Eltink <geert.eltink@gmail.com>
2023-11-08 20:53:27 +0100

    fix: use proper image importing

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,15 +2,16 @@ import { defineCollection, z } from 'astro:content';
 
 const notes = defineCollection({
   type: 'content',
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    published: z.coerce.date(),
-    modified: z.coerce.date().optional(),
-    tags: z.array(z.string()).optional(),
-    coverImage: z.string().optional(),
-    coverImageAlt: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      published: z.coerce.date(),
+      modified: z.coerce.date().optional(),
+      tags: z.array(z.string()).optional(),
+      coverImage: image().optional(),
+      coverImageAlt: z.string().optional(),
+    }),
 });
 
 // Export a single `collections` object to register your collection(s)
