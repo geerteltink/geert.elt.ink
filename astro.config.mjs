7ae309ee13e68caaa2c959f15629010d67c6f44c
Geert Eltink <geert.eltink@gmail.com>
2023-11-18 17:58:05 +0100

    feat: vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel/static';
 import vue from '@astrojs/vue';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://geert.elt.ink',
-  integrations: [mdx(), sitemap(), vue()],
+    site: 'https://geert.elt.ink',
+    integrations: [mdx(), sitemap(), vue()],
+    output: 'static',
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true,
+        },
+        speedInsights: {
+            enabled: true,
+        },
+    }),
 });
