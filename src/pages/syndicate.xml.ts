460cb1c7c78baa064845148a1ee9b81c11de763d
Geert Eltink <geert.eltink@gmail.com>
2023-11-18 17:34:08 +0100

    fix: import order

--- src/pages/syndicate.xml.ts
+++ src/pages/syndicate.xml.ts
@@ -1,8 +1,8 @@
 import rss from '@astrojs/rss';
 import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
-import sanitizeHtml from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
+import sanitizeHtml from 'sanitize-html';
 const parser = new MarkdownIt();
 
 function sortPosts(a: { data: { published: Date } }, b: { data: { published: Date } }) {
