a6dc14594451f8ec3436c99ae52daffbd6f58b66
Geert Eltink <geert.eltink@gmail.com>
2024-01-06 15:13:47 +0100

    feat: make relative date dynamic

---
+++ src/components/RelativeDate.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { formatDistanceToNow, formatISO } from 'date-fns';
+
+const props = defineProps({
+  date: {
+    type: Date,
+    required: true,
+  },
+});
+
+const isoDate = formatISO(props.date);
+
+const relativeDate = formatDistanceToNow(props.date, {
+  addSuffix: true,
+});
+</script>
+
+<template>
+  <time :datetime="isoDate">
+    {{ relativeDate }}
+  </time>
+</template>
