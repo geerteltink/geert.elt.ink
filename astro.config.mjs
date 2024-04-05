fd3faee56c6e29eaa52eb0d1541fa64d323bd7d7
Geert Eltink <geert.eltink@gmail.com>
2024-04-05 07:57:36 +0200

    feat: switch to vercel speed-insights

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/static';
 import vue from '@astrojs/vue';
 import { defineConfig } from 'astro/config';
 
@@ -8,12 +7,4 @@ export default defineConfig({
   site: 'https://geert.elt.ink',
   integrations: [mdx(), sitemap(), vue()],
   output: 'static',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    speedInsights: {
-      enabled: true,
-    },
-  }),
 });
