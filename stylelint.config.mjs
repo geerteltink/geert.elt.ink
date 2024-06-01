0d5c827c790b14d27c902ee763be7a485f5c33d4
Geert Eltink <geert.eltink@gmail.com>
2024-06-01 14:29:18 +0200

    feat: stylelint.config.mjs

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,9 +1,9 @@
-module.exports = {
+const config = {
   extends: ['stylelint-config-recommended', 'stylelint-config-recess-order'],
-
   plugins: ['stylelint-order'],
-
   rules: {
     'max-nesting-depth': 2,
   },
 };
+
+export default config;
