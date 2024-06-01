b7dd4858e143b24292638b7bc97413afc3db5a7a
Geert Eltink <geert.eltink@gmail.com>
2024-06-01 14:37:56 +0200

    feat: prettier.config.mjs

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,4 +1,5 @@
-module.exports = {
+/** @type {import("prettier").Config} */
+const config = {
   trailingComma: 'es5',
   tabWidth: 2,
   semi: true,
@@ -26,3 +27,5 @@ module.exports = {
     },
   ],
 };
+
+export default config;
